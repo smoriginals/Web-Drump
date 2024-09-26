893b40dd7151ed3307375f9fbb6a223cbdfc9ae3
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-09-26 22:16:43 +0530

    App is Ready
    Complete.

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+let btn=document.querySelectorAll(".btns");
+
 window.addEventListener("keydown",(e)=>{
-	let keys=document.querySelector(`audio[dat-key="${e.keyCode}"]`);
-	keys.play();
+	let audio=document.querySelector(`audio[dat-key="${e.keyCode}"]`);
+	let buttons=document.querySelector(`.btns[dat-key="${e.keyCode}"]`);
+	
+	if(!audio){return;}
+
+	audio.currentTime=0;
+	audio.play();
+	buttons.classList.add("animateB");
 });
 
+
+function removeanim(evt){
+	if(evt.propertyName!=="transform"){
+		return;
+	}
+	this.classList.remove("animateB");
+}
+
+btn.forEach(buttons => buttons.addEventListener("transitionend",removeanim));
